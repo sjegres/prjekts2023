36e295734028d7f37e3579d2ac50382c32738e97
dacebavikina2 <dacebavikina2@gmail.com>
2023-10-10 08:00:32 +0300

    ass
    aa

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "proj2023";
+$mysqli = new mysqli($servername, $username, $password, $db);
+?>
--- proc.php
+++ proc.php
@@ -1,6 +1,15 @@
 <?php
 $l = $_POST["login"];
 $p = $_POST["password"];
-echo "$l, HELLO, $p";
+
+include('cfg.php');
+$result = $mysqli->query("SELECT * FROM users WHERE login=$l");
+if (mysqli_num_rows($result) > 0){
+    echo "GOOOOD!!!!";
+}
+else{
+    echo "BYE BYE!!!";
+}
+
 
 ?>
